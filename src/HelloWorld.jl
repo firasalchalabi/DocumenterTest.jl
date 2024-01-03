7c87a66cccc6c153ff869fd2f670cd0e9bffb07b
Firas <firas_alchalabi@hotmail.com>
2024-01-03 15:34:29 -0700

    Added documentation to a function that is not exported.

--- src/HelloWorld.jl
+++ src/HelloWorld.jl
@@ -8,3 +8,17 @@ function hello_world()
   println("Hello, world!")
 
 end
+
+"""
+    non_exported_func(x::Integer)
+
+Prints the integer x to stdout.
+
+# Arguments
+ - `x::Integer`: the number to print to stdout.
+"""
+function non_exported_func(x::Integer)
+
+  println(x)
+
+end
